3aa87368251196fba62477018b737ab08b44c326
Shankar-Kb <shankar.kb95@gmail.com>
2021-03-08 00:32:02 +0530

    finalized the build

--- index.js
+++ index.js
@@ -54,55 +54,53 @@ app.post('/create-message', async (req, res) => {
         }
         await db.collection('secretMessage').insertOne(data);
         const result = await db.collection('secretMessage').findOne({key: data.key});
-        const userMailUrl = `${req.body.targetURL}?rs=${result._id}`;
+        const userMailUrl = `${req.body.targetURL}/msg?result_id=${result._id}`;
         mailData.to = req.body.targetMail;
         mailData.html = mailMessage(userMailUrl);
         await transporter.sendMail(mailData);
+        client.close();
         res.status(200).json({message: "secret message is sent. Don't forget yout secret key and password", result});
-
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
-    } finally {
-        client.close();
     }
 })
 
 app.get('/message-by-id/:id', async (req, res) => {
     try {
-        const client = await mongoClient.connect(DB_URL);
+        const client = await mongoClient.connect(DB_URL, {useUnifiedTopology: true});
         const db = client.db('secretMessageDatabase');
         const result = await db.collection('secretMessage').find({_id: objectId(req.params.id)}).project({password: 0, _id: 0, key: 0}).toArray();
+        client.close();
         res.status(200).json({message: "message has been fetched successfully", result});
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
-    } finally {
-        client.close();
     }
 })
 
 app.delete('/delete-message', async (req, res) => {
     try {
-        const client = await mongoClient.connect(DB_URL);
+        const client = await mongoClient.connect(DB_URL, {useUnifiedTopology: true});
         const db = client.db('secretMessageDatabase');
         const secret = await db.collection('secretMessage').findOne({key: req.body.secretKey});
         if(secret){
             const compare = await bcrypt.compare(req.body.password, secret.password);
             if (compare){
                 await db.collection('secretMessage').findOneAndDelete({key: req.body.secretKey});
+                client.close();
                 res.status(200).json({message: "message has been deleted successfully"});
             }else{
+                client.close();
                 res.status(401).json({message: "incorrect password!"});
             }
         }else{
+            client.close();
             res.status(404).json({message: "secret key not found!!!"});
         }
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
-    } finally{
-        client.close();
     }
 })
 
